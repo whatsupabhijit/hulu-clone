df607119f1e3fe4c1cfaaf4114f69468312417b3
unknown <whatsupabhijit@gmail.com>
2020-08-30 01:01:16 +0530

    Header 90% completed. Added Material UI Icons and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import "./Header.js";
+import Header from "./Header.js";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      {/* Header */}
+      <Header />
+      {/* Navigations */}
+
+      {/* Results */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Header.css";
+import HomeIcon from "@material-ui/icons/Home";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlinecon from "@material-ui/icons/PersonOutline";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__icons">
+        {/* All icons will go here */}
+        <div className="header__icon">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+
+        <div className="header__icon">
+          <FlashOnIcon />
+          <p>Trending</p>
+        </div>
+
+        <div className="header__icon">
+          <LiveTvIcon />
+          <p>Verified</p>
+        </div>
+
+        <div className="header__icon">
+          <VideoLibraryIcon />
+          <p>Collections</p>
+        </div>
+
+        <div className="header__icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+
+        <div className="header__icon">
+          <PersonOutlinecon />
+          <p>Account</p>
+        </div>
+      </div>
+
+      {/* The logo goes here */}
+      <img
+        src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
+        alt="hulu clone logo"
+        className="header__logo"
+      />
+    </div>
+  );
+}
+
+export default Header;
